84a4894f990275865627e4ff050a7d97759b2fd5
jeon-hakyung <jhakyung12@gmail.com>
2025-06-05 12:25:32 +0900

    묘우묘우 초기세팅

---
+++ src/main/java/com/example/myoumyouback/MyoumyoubackApplication.java
@@ -0,0 +1,13 @@
+package com.example.myoumyouback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoumyoubackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyoumyoubackApplication.class, args);
+	}
+
+}
